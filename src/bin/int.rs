584ea9b5b4dd12f3d3836b7bae8714c8d89504ad
yy-tromb <107352731+yy-tromb@users.noreply.github.com>
2025-07-14 01:00:28 +0900

    Impl mode ( '[0,1)' or '[0,n)' (n is given by arg) )

--- src/bin/int.rs
+++
@@ -1,8 +0,0 @@
-use rand::Rng;
-
-fn main() {
-    let mut rng = rand::rng();
-    let r1 :f64 = rng.random();
-    let by :i64 = std::env::args().skip(1).next().unwrap().parse().unwrap();
-    println!("{}",r1*(by as f64));
-}
--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,15 @@ use rand::Rng;
 
 fn main() {
     let mut rng = rand::rng();
-    let r1 : f64 = rng.random();
-    println!("{}",r1);
+    match std::env::args().skip(1).next(){
+        Some(by) => {
+            let by: i64 = by.parse().expect("Failed to parse as int");
+            let r1: f64 = rng.random();
+            println!("{}",r1*(by as f64));
+        },
+        None => {
+            let r1: f64 = rng.random();
+            println!("{}",r1);
+        }
+    };
 }

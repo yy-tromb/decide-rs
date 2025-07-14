0e8800d5ed85a33ef58d32e69a81da52a2f34ba2
yy-tromb <107352731+yy-tromb@users.noreply.github.com>
2025-07-14 14:09:56 +0900

    Fix int mode for input:uint, output:uint

--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,9 @@ fn main() {
     let mut rng = rand::rng();
     match std::env::args().skip(1).next(){
         Some(by) => {
-            let by: i64 = by.parse().expect("Failed to parse as int");
+            let by: u64 = by.parse().expect("Failed to parse as int");
             let r1: f64 = rng.random();
-            println!("{}",r1*(by as f64));
+            println!("{}",(r1*(by as f64).floor())as u64);
         },
         None => {
             let r1: f64 = rng.random();
